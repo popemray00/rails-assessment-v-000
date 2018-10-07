f0eac20f419bb9c671e9f3c02353af499d156e5a
Michael Pope <popemray1986@gmail.com>
2018-10-07 18:29:32 +0000

    Done.

---
+++ concerts/app/models/concert.rb
@@ -0,0 +1,22 @@
+class Concert < ApplicationRecord
+  has_many :tickets
+has_many :users,through: :tickets
+
+
+def age_req
+  if min_age >= 21
+    "Adults Only"
+  elsif min_age <= 20
+    "All ages"
+  end
+end
+
+def showtime
+  time.strftime('%m/%d/%Y at %I:%M %p')
+end
+
+def self.ratingscope(age)
+  where("min_age <=  ?", age)
+end
+
+end
---
+++ concerts/db/migrate/20181007182037_create_concerts.rb
@@ -0,0 +1,12 @@
+class CreateConcerts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :concerts do |t|
+      t.string :title
+      t.integer :min_age
+      t.integer :cost, :default => 45
+      t.datetime :time
+
+      t.timestamps
+    end
+  end
+end
