897ccb977fd5687732bee200d4a39ad427bbc52b
Michael Pope <popemray1986@gmail.com>
2018-10-07 18:54:00 +0000

    Done.

--- concerts/config/routes.rb
+++ concerts/config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  resources :tickets
-  resources :concerts
+  get '/auth/facebook/callback' => 'sessions#create'
+  root 'users#home'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#login'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get '/logout', to: 'sessions#destroy'
+
+  post '/buyticket', to: 'concerts#buy_tickets'
+
+  post '/refund', to: 'concerts#refund'
+  resources :users, only: [:show, :new, :create] do
+  resources :concerts, only: [:show, :index, :new]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- concerts/db/migrate/20181007184447_create_tickets.rb
+++ concerts/db/migrate/20181007184447_create_tickets.rb
@@ -1,6 +1,8 @@
 class CreateTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :tickets do |t|
+     t.integer :user_id
+     t.integer :concert_id
 
       t.timestamps
     end
