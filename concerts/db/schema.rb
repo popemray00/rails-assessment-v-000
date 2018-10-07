efea3a7e0aa0de2498ab81ecf9473a67ab41d405
Michael Pope <popemray1986@gmail.com>
2018-10-07 18:53:51 +0000

    automatically backed up by learn

--- concerts/db/schema.rb
+++ concerts/db/schema.rb
@@ -10,13 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_182037) do
+ActiveRecord::Schema.define(version: 2018_10_07_184848) do
 
   create_table "concerts", force: :cascade do |t|
     t.string "title"
     t.integer "min_age"
     t.integer "cost", default: 45
     t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "concert_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "age"
+    t.integer "money"
+    t.integer "100"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
