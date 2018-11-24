3352c182c4a72090d60128d5074fa4e620e93958
popemray00 <popemray@yahoo.com>
2018-11-24 11:50:11 -0800

    added ajax get request to user.js

--- concerts/app/assets/javascripts/users.js
+++ concerts/app/assets/javascripts/users.js
@@ -1,6 +1,15 @@
 $(function(){
  $("a.load_concerts").on("click", function(e){
-    alert("you clicked this link")
+    
+    $.ajax({
+        method: "GET",
+        url: this.href
+    }).done(function(data){
+        
+        console.log(data)
+    });
+
+
     e.preventDefault();
  })
 })
